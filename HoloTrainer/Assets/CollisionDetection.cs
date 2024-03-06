0aa012d0de29cb3a9de577323b0a08de117a6dc0
Leonardo Pollicino <144808211+Bourne47@users.noreply.github.com>
2024-03-06 12:14:49 +0100

    Update CollisionDetection.cs
    removed static

--- HoloTrainer/Assets/CollisionDetection.cs
+++ HoloTrainer/Assets/CollisionDetection.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 */
 public class CollisionDetection : MonoBehaviour
 {
-    public static int number_target = 0;//number of times the sphere collided with the cube
+    public int number_target = 0;//number of times the sphere collided with the cube
 
     /** OnTriggerEnter is called every time a collision happens
     * checks if the cube (with tag "target") is colliding
